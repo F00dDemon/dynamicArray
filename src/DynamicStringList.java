eedbb2fb6d90b87dd19c1dfe3d78ec430dfa92c8
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-01-08 14:41:43 -0800

    divided by individual share

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,35 +1,36 @@
 public class DynamicStringList implements StringList {
-
+    // Orion
     @Override
     public String get(int index) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'get'");
+        
     }
-
+    //Fred
     @Override
     public void set(int index, String value) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'set'");
     }
-
+    // Orion
     @Override
     public void add(String value) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
-
+    //Fred
     @Override
     public String remove(int index) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'remove'");
     }
-
+    // Orion
     @Override
     public int size() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'size'");
     }
-
+    //Fred
     @Override
     public int capacity() {
         // TODO Auto-generated method stub
