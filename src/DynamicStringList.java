af6726802944eb504b2888ce2938311ac5ab9c6b
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-01-11 19:57:46 -0800

    done with publicint capacity,

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -11,9 +11,8 @@ public class DynamicStringList implements StringList {
     public void set(int index, String value) {
         // TODO Auto-generated method stub
         // throw new UnsupportedOperationException("Unimplemented method 'set'");
+    
 
-
-    }
     // Orion
     @Override
     public void add(String value) {
@@ -48,7 +47,7 @@ public class DynamicStringList implements StringList {
     public int capacity() {
         // TODO Auto-generated method stub
         // throw new UnsupportedOperationException("Unimplemented method 'capacity'");
-       
+        return data.length;
 
 
 
